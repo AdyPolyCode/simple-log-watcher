9f6caf913b21e81224d11e3e7b4e26ef068c6fa3
Ady <76212893+AdyPolyCode@users.noreply.github.com>
2023-01-05 21:07:01 +0100

    init setup of child processes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// TODO: handle main processes
+const { fork } = require('child_process');
+
+// TODO: add all handlers
+function runProcesses() {
+  const watcher = fork('./watcher/index.js', { killSignal: 'SIGINT' });
+}
+
+runProcesses();
+
+process.on('uncaughtException', (error, origin) => {
+  console.error(error.message);
+});
