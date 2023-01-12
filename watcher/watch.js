0de0f45578a5c74b538b280b78eb002e2b7f8874
Ady <76212893+AdyPolyCode@users.noreply.github.com>
2023-01-12 19:18:21 +0100

    fix single instance pattern & remove UNEC methods (pub/sub)

--- watcher/watch.js
+++ watcher/watch.js
@@ -3,76 +3,103 @@ const { EventEmitter } = require('events');
 
 const { FileHandler } = require('./file-handler');
 
+/**
+ *
+ */
 class FileWatcher extends EventEmitter {
-    instance;
+    static instance;
     fileName;
     filePath;
+    eventHandler;
 
-    constructor(fileName, filePath) {
-        super({ captureRejections: true });
-        this.fileName = fileName;
-        this.filePath = filePath;
-    }
-
-    static watch() {
-        if (!this.instance) {
-            const { fileToWatch, config } = FileHandler._getFile();
-
-            this.instance = new FileWatcher(config.filename, config.path);
-
-            /**
-             *
-             * @param {*} err
-             * @param {*} bytes
-             * @param {*} buffer
-             */
-            const readHandler = (err, bytes, buffer) => {
-                if (err) throw err;
-
-                if (bytes) {
-                    console.log(buffer.toString('utf8'));
-                }
-            };
-
-            /**
-             *
-             * @param {*} fd
-             */
-            const reader = (fd) => {
-                read(fd, readHandler);
-            };
-
-            /**
-             *
-             */
-            open(fileToWatch, (err, fd) => {
-                if (err) throw err;
-
-                const interval = this.instance._watchHelper(fd, 5000, reader);
-
-                this.instance.on('close-watch', () => {
-                    console.info(
-                        `Closing file: ${this.instance.fileName} at path: ${this.instance.filePath}`
-                    );
-                    clearInterval(interval);
-                    close(fd, (err) => {
-                        if (err) throw err;
-                    });
-                });
-            });
-
-            return this.instance;
+    /**
+     *
+     * @param {String} fileName
+     * @param {String} filePath
+     * @returns FileWatcher instance
+     */
+    constructor(fileName, filePath, eventHandler) {
+        if (!FileWatcher.instance) {
+            super({ captureRejections: true });
+            this.fileName = fileName;
+            this.filePath = filePath;
+            this.eventHandler = eventHandler;
+            FileWatcher.instance = this;
         }
-
-        return this.instance;
+        return FileWatcher.instance;
     }
 
     /**
      *
-     * @param {*} fd
-     * @param {*} time
-     * @param {*} cb
-     * @returns
+     * @returns FileWatcher instance
+     */
+    static init(eventHandler) {
+        if (!FileWatcher.instance) {
+            const {
+                config: { filename, path },
+            } = FileHandler._getFile();
+
+            FileWatcher.instance = new FileWatcher(
+                filename,
+                path,
+                eventHandler
+            );
+        }
+        return FileWatcher.instance;
+    }
+
+    /**
+     *
+     */
+    watch() {
+        const { fileToWatch } = FileHandler._getFile();
+        /**
+         *
+         * @param {Object} err
+         * @param {Number} bytes
+         * @param {ArrayBuffer} buffer
+         */
+        const readHandler = (err, bytes, buffer) => {
+            if (err) throw err;
+
+            if (bytes) {
+                const data = buffer.toString('utf8');
+
+                this.eventHandler.emit('watch-data', data);
+            }
+        };
+
+        /**
+         *
+         * @param {Number} fd
+         */
+        const reader = (fd) => {
+            read(fd, readHandler);
+        };
+
+        open(fileToWatch, (err, fd) => {
+            if (err) throw err;
+
+            const interval = this._watchHelper(fd, 5000, reader);
+
+            this.on('close-watch', () => {
+                console.info(
+                    `Closing file: ${this.fileName} at path: ${this.filePath}`
+                );
+                clearInterval(interval);
+                close(fd, (err) => {
+                    if (err) throw err;
+                });
+            });
+        });
+    }
+
+    /**
+     *
+     * @param {Number} fd
+     * @param {Number} time
+     * @param {Function} cb
+     * @returns Async interval
      */
     _watchHelper(fd, time, cb) {
         const interval = setInterval(() => {
