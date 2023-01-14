2c7124894f9e9e3ef0a835c878e98579a10af72e
Ady <76212893+AdyPolyCode@users.noreply.github.com>
2023-01-14 19:39:40 +0100

    refactor to proper server module setup

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
+
 const AppFactory = require('./app-factory');
 const Server = require('./server');
 const Watcher = require('./watcher');
 const { ServerEventHandler } = require('./broker/server-event-handler');
 
-function runProcesses() {
-  const { server, watcher } = AppFactory.create(
-    Server,
-    Watcher,
-    ServerEventHandler
-  );
-
-  watcher.start();
-  server.start();
+function createServerModules(appFactory) {
+  return appFactory.create(Server, Watcher, ServerEventHandler);
 }
 
-runProcesses();
+const { server, watcher } = createServerModules(AppFactory);
+
+try {
+  // server.start()
+  watcher.start();
+} catch (error) {
+  console.log('Error:, ', error);
+}
